3c8a139a93a4b6f682af1ecfe7660d2387caf642
Noah Altunian <noah@cirruslabs.io>
2018-04-27 20:13:18 -0400

    Add controller [Work In Progress]

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 import redis
 
+################################################################
+#                       LOG SYMBOLS                            #
+# [*] a standard message between the client and the server     #
+# [!] indicates an action has taken place from the client-side #
+################################################################
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
@@ -29,14 +35,23 @@ def simulation():
 # SocketIO FUNCTIONS #
 ######################
 @socketio.on('connection')
-def handle_connection(data):
-    print(data)
+def handle_connection(connMsg):
+    # set the view by grabbing the last word in the returned message
+    view = connMsg.split() # first we need to turn the string into a list
+    view = view[-1] # now we grab the last value
+    redis.set('currentView', view)
+    print(connMsg)
+
+@socketio.on('latest_view')
+def handle_view(view):
+    # store the latest view for checking
+    redis.set('requestedView', view)
+    print('[!] The latest view is ' + view)
 
 if __name__ == '__main__':
     ###############
     # GLOBAL VARS #
     ###############
-    global view
     global redis
     redis = redis.StrictRedis(host='localhost', port=6379)
     socketio.run(app, debug=True)
